14b6bfd5ae4ef49e96eff0e2471539253252b3be
vwestberg <vwestberg@lselectric.com>
2026-05-21 10:38:53 -0500

    Enable DMS lock-screen fingerprint when scanner is enabled

--- settings/fingerprint.nix
+++ settings/fingerprint.nix
@@ -1,6 +1,11 @@
 { config, lib, ... }:
 {
-  config = {
-    eiros.system.hardware.fingerprint_scanner.enable = lib.mkDefault true;
-  };
+  config = lib.mkMerge [
+    {
+      eiros.system.hardware.fingerprint_scanner.enable = lib.mkDefault true;
+    }
+    (lib.mkIf config.eiros.system.hardware.fingerprint_scanner.enable {
+      eiros.system.user_defaults.dms.lock_screen.fprint.enable = lib.mkDefault true;
+    })
+  ];
 }
